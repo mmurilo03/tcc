1846fafa9d10f429fc90f9bd5fdbc3fe99161c13
mmurilo03 <murilo.ferreira@academico.ifpb.edu.br>
2024-03-15 16:04:59 -0300

    Exibindo canvas na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { game } from "../gameEngine/main";
+import { useEffect, useRef } from "react";
 
 function App() {
+    const canvasRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!canvasRef.current) return
+            canvasRef.current.replaceChildren(game.canvas)
+        
+    })
 
     return (
         <>
+            <div className="canvas" ref={canvasRef}></div>
         </>
     );
 }
